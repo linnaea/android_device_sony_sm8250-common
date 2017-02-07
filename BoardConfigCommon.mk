2f64e02d831f1dd292e78c8a9b4db285561a5e45
Peng Xu <pxu@qca.qualcomm.com>
2017-02-07 15:16:52 -0800

    sm8250-common: Enable support for IEEE80211AX
     * This is to allow supporting WiFi 6 mobile hotspots,
       to be noted this is only supported on SDM855+ platforms.
    
    Change-Id: Iab61bd12499968e8823b3078acd1c7b8bce46136

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -246,6 +246,7 @@ WIFI_HIDL_UNIFIED_SUPPLICANT_SERVICE_RC_ENTRY := true
 WPA_SUPPLICANT_VERSION := VER_0_8_X
 CONFIG_ACS := true
 CONFIG_IEEE80211AC := true
+CONFIG_IEEE80211AX := true
 
 # Include the proprietary files BoardConfig.
 include vendor/sony/sm8250-common/BoardConfigVendor.mk

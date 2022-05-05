1f594b7575d83e42986d178fbf85ee720791a73e
Alexander Koskovich <akoskovich@pm.me>
2022-05-04 18:13:03 -0700

    sm8250-common: Drop some audio packages
     * All of these are implied, except libcirrusspkrprot
       which was removed as we are not using any Cirrus
       hardware, and even hardware supported by it is hard
       to find in production.
    
    Change-Id: Iad7824105bd2c22899365f8a5fc0a9e73d36beda

--- edo.mk
+++ edo.mk
@@ -134,26 +134,17 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     liba2dpoffload \
-    libaudiopreprocessing \
     libbatterylistener \
-    libbundlewrapper \
-    libcirrusspkrprot \
     libcomprcapture \
-    libdownmix \
-    libdynproc \
-    libeffectproxy \
     libexthwplugin \
     libhdmiedid \
     libhfp \
-    libldnhncr \
     libqcompostprocbundle \
     libqcomvisualizer \
     libqcomvoiceprocessing \
-    libreverbwrapper \
     libsndmonitor \
     libspkrprot \
     libssrec \
-    libvisualizer \
     libvolumelistener
 
 # Audio Policies

466026421a97ca5983bed8d2861d958e0f10c184
RONIN808 <shubhamshedage808@gmail.com>
2023-03-26 06:56:51 +0530

    sm8250-common: build android.hidl.allocator@1.0
    Signed-off-by: karthik1896 <karthik1896@outlook.com>

--- edo.mk
+++ edo.mk
@@ -294,6 +294,8 @@ PRODUCT_PACKAGES += \
 
 # HIDL
 PRODUCT_PACKAGES += \
+    android.hidl.allocator@1.0 \
+    android.hidl.allocator@1.0.vendor \
     android.hidl.base@1.0 \
     libhidltransport.vendor \
     libhwbinder.vendor
